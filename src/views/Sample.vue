a63116a7a8ab3b5f4660e95a71256e08a19a18fa
austin shields <ajshields196@gmail.com>
2021-03-31 20:42:14 -0400

    add sample page and route

---
+++ src/views/Sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Sample Sausage Yum"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
